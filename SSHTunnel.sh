0c7b5b8316765ad1c69cc07859818cc7d89c4b69
Archer Reilly <1246506786@qq.com>
2016-05-19 22:45:01 +0800

    fuck network

--- SSHTunnel.sh
+++ SSHTunnel.sh
@@ -5,6 +5,7 @@ do
   then
     echo "autossh service running, everything is fine"
   else
+    autossh -fNR 8888:localhost:22 archer@115.28.70.69
     echo "autossh is not running"
   fi
   sleep 60
